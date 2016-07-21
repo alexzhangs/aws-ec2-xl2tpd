bcd878c917120bc63e10f4dc044a0794b9b0bb09
alex <alexzhangs@gmail.com>
2016-07-22 02:38:55 +0800

    Add l2tp-ec2-install.sh and conf

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # l2tp-server
+
+## Installation
+
+### On an AWS EC2 server
+
+git clone https://github.com/alexzhangs/l2tp-server.git
+
+sudo sh l2tp-server/l2tp-ec2-install.sh
---
+++ l2tp-ec2-install.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+IPSEC_PSK=${1:?}
+
+L2TP_PORT=1701
+L2TP_VIRTUAL_IP=192.168.42.1
+L2TP_DHCP_CIDR=192.168.42.0/24
+L2TP_DHCP_START=192.168.42.10
+L2TP_DHCP_END=192.168.42.250
+PRIMARY_DNS=8.8.8.8
+SECOND_DNS=8.8.4.4
+
+# Those two variables will be found automatically
+SERVER_PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4` || exit $?
+SERVER_PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4` || exit $?
+
+sysctl_permanent () {
+    local key=${1:?}
+    local value=${2:$?}
+    if grep -q "^$key = $value" /etc/sysctl.conf; then
+        sysctl -w $key=$value
+        return $?
+    fi
+
+    sed -i "/^$key = /d" /etc/sysctl.conf || return $?
+    sysctl -w $key=$value >> /etc/sysctl.conf || return $?
+}
+
+
+yum install -y --enablerepo=epel openswan xl2tpd || exit $?
+
+# ipsec.conf
+/bin/cp -a ${0%/*}/conf/ipsec.conf /etc/ipsec.conf || exit $?
+sed -i "s/<SERVER_PRIVATE_IP>/$SERVER_PRIVATE_IP/" /etc/ipsec.conf || exit $?
+sed -i "s/<SERVER_PUBLIC_IP>/$SERVER_PUBLIC_IP" /etc/ipsec.conf || exit $?
+sed -i "s/<L2TP_PORT>/$L2TP_PORT" /etc/ipsec.conf || exit $?
+
+# ipsec.secrets
+/bin/cp -a ${0%/*}/conf/ipsec.secrets /etc/ipsec.secrets || exit $?
+sed -i "s/<SERVER_PUBLIC_IP>/$SERVER_PUBLIC_IP" /etc/ipsec.secrets || exit $?
+sed -i "s/<IPSEC_PSK>/$IPSEC_PSK/" /etc/ipsec.secrets || exit $?
+
+# xl2tpd.conf
+/bin/cp -a ${0%/*}/conf/xl2tpd.conf /etc/xl2tpd/xl2tpd.conf || exit $?
+sed -i "s/<L2TP_VIRTUAL_IP>/$L2TP_VIRTUAL_IP" /etc/xl2tpd/xl2tpd.conf || exit $?
+sed -i "s/<L2TP_DHCP_START>/$L2TP_DHCP_START/" /etc/xl2tpd/xl2tpd.conf || exit $?
+sed -i "s/<L2TP_DHCP_END>/$L2TP_DHCP_END/" /etc/xl2tpd/xl2tpd.conf || exit $?
+sed -i "s/<L2TP_PORT>/$L2TP_PORT" /etc/xl2tpd/xl2tpd.conf || exit $?
+
+# options.xl2tpd
+/bin/cp -a ${0%/*}/conf/options.xl2tpd /etc/ppp/options.xl2tpd || exit $?
+sed -i "s/<PRIMARY_DNS>/$PRIMARY_DNS" /etc/ppp/options.xl2tpd || exit $?
+sed -i "s/<SECOND_DNS>/$SECOND_DNS" /etc/ppp/options.xl2tpd || exit $?
+
+# char-secrets
+/bin/cp -a ${0%/*}/conf/chap-secrets /etc/ppp/char-secrets || exit $?
+
+# sysctl
+sysctl_permanent 'net.ipv4.ip_forward' 1 || exit $?
+
+# iptables
+service iptables status | grep "$L2TP_DHCP_CIDR" | grep -q 'MASQUERADE'
+if [[ $? -ne 0 ]]; then
+    iptables -t nat -A POSTROUTING -s "$L2TP_DHCP_CIDR" -o eth0 -j MASQUERADE || exit $?
+    service iptables save || exit $?
+fi
+
+# services
+service ipsec restart || exit $?
+service xl2tpd restart || exit $?
+chkconfig ipsec on || exit $?
+chkconfig xl2tpd on || exit $?
+
+exit
